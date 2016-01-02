504f937eb1081d3e85a589f8f2d1625137c7ac63
zemiak <zemiak@gmail.com>
2016-01-02 07:22:21 +0100

    Serving v1 of static files for slovakradio

---
+++ server/v1/js/application.js
@@ -0,0 +1,41 @@
+var resourceLoader;
+
+App.onLaunch = function(options) {
+    var javascriptFiles = [
+        `${options.BaseUrl}js/Presenter.js`,
+        `${options.BaseUrl}js/ResourceLoader.js`,
+        `${options.BaseUrl}js/RadioData.js`
+    ];
+    
+    evaluateScripts(javascriptFiles, function(success) {
+        if(success) {
+            resourceLoader = new ResourceLoader(options.BaseUrl);
+            resourceLoader.loadResource(`${options.BaseUrl}templates/Main.xml.js`, function(resource) {
+                Presenter.BaseUrl = options.BaseUrl;
+                var doc = Presenter.makeDocument(resource);
+                doc.addEventListener("select", Presenter.load.bind(Presenter));
+                Presenter.pushDocument(doc);
+            });
+        } else {
+            var errorDoc = createAlert("Evaluate Scripts Error", "Error attempting to evaluate external JavaScript files.");
+            navigationDocument.presentModal(errorDoc);
+        }
+    });
+}
+
+
+var createAlert = function(title, description) {
+    var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
+    <document>
+    <alertTemplate>
+    <title>${title}</title>
+    <description>${description}</description>
+    </alertTemplate>
+    </document>`
+
+    var parser = new DOMParser();
+
+    var alertDoc = parser.parseFromString(alertString, "application/xml");
+
+    return alertDoc
+}
---
+++ server/v1/templates/Info.xml.js
@@ -0,0 +1,12 @@
+var Template = function() { return `<document>
+<alertTemplate>
+    <title>Info</title>
+    <description>
+        Jednoduchý prehrávač rádií RTVS. Artwork (C) RTVS, SITA.
+    </description>
+    <button template="Main">
+        <text>OK</text>
+    </button>
+</alertTemplate></document>
+`;
+}
