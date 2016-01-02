58fabc3871794706aa6cf4a8a6bc49604bbf7a1b
zemiak <zemiak@gmail.com>
2016-01-02 07:22:21 +0100

    Serving v1 of static files for slovakradio

---
+++ server/v1/templates/Settings.xml.js
@@ -0,0 +1,12 @@
+var Template = function() { return `<document>
+<alertTemplate>
+    <title>Nastavenia</title>
+    <description>
+        Neimplementované.
+    </description>
+    <button template="Main">
+        <text>OK</text>
+    </button>
+</alertTemplate></document>
+`;
+}
